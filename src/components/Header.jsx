178486d7638f4901929555a1e42760c156d2d1cf
haitn10 <tranhaihk00@gmail.com>
2022-06-21 02:01:07 +0700

    add items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import { app } from '../firebase.config';
 
 import Logo from '../img/logo.png';
 import Avatar from '../img/avatar.png';
-import { useStateValue } from '../context/StateProvider';
+import { useStateValue } from '../context/StateProvider';   
 import { actionType } from '../context/reducer';
 
 const Header = () => {
@@ -44,7 +44,7 @@ const Header = () => {
     }
 
   return (
-    <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16'>
+    <header className='fixed z-50 w-screen p-3 px-4 md:p-6 md:px-16 bg-primary'>
         {/* desktop & tablet */}
         <div className='hidden md:flex w-full h-full items-center justify-between'>
             <Link to={'/'} className='flex items-center gap-2'>
@@ -103,7 +103,8 @@ const Header = () => {
                         {user && user.email === "tranhaihk00@gmail.com" && (
                             <Link to={'/createItem'}>
                                 <p className='px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 
-                                transition-all duration-100 ease-in-out text-textColor text-base'>
+                                transition-all duration-100 ease-in-out text-textColor text-base'
+                                >
                                 New Item <MdAdd />
                                 </p>
                             </Link>
@@ -162,19 +163,23 @@ const Header = () => {
 
                     <ul className='flex flex-col'>
                         <li className='text-base text-textColor hover:text-headingColor duration-100
-                        transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>
+                        transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                        onClick={() => setisMenu(false)}>
                             Home
                         </li>
                         <li className='text-base text-textColor hover:text-headingColor duration-100
-                        transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>
+                        transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                        onClick={() => setisMenu(false)}>
                             Menu
                         </li>
                         <li className='text-base text-textColor hover:text-headingColor duration-100
-                        transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>
+                        transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                        onClick={() => setisMenu(false)}>
                             About Us
                         </li>
                         <li className='text-base text-textColor hover:text-headingColor duration-100
-                        transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'>
+                        transition-all ease-in-out cursor-pointer hover:bg-slate-100 px-4 py-2'
+                        onClick={() => setisMenu(false)}>
                             Service
                         </li>
                     </ul>
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -43,19 +43,20 @@ const HomeContainer = () => {
                 alt='hero-bg'
             />
 
-            <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center px-32 py-4'>
+            <div className='w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32 py-4
+            gap-4 flex-wrap'>
                 {heroData && 
                     heroData.map((n) => (
                         <div
                         key={n.id}
-                        className='w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center
-                        justify-center'>
-                            <img src={n.imageSrc} className='w-40 -mt-20' alt='i1'/>
-                            <p className='text-xl font-semibold text-textColor mt-4'>
+                        className=' lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center
+                        justify-center drop-shadow-lg'>
+                            <img src={n.imageSrc} className='w20 lg:w-40 -mt-10 lg:-mt-20' alt='I1'/>
+                            <p className='text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4'>
                             {n.name}
                             </p>
                             
-                            <p className='text-sm text-lighttextGray font-semibold my-3'>
+                            <p className='text-[12px] lg:text-sm text-lighttextGray font-semibold my-1 lg:my-3'>
                             {n.decp}
                             </p>
 
