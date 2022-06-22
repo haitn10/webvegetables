67a846020b20e0e38f4624e4421054329325736a
haitn10 <tranhaihk00@gmail.com>
2022-06-22 11:00:46 +0700

    append item

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
     const firebaseAuth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
-    const [{user}, dispath] = useStateValue();
+    const [{user, cartShow, cartItems}, dispath] = useStateValue();
 
     const [isMenu, setisMenu]= useState(false)
     
@@ -40,6 +40,13 @@ const Header = () => {
         dispath({
             type: actionType.SET_USER,
             user: null,
+        });
+    }
+
+    const showCart = () => {
+        dispath ({
+            type : actionType.SET_CART_SHOW,
+            cartShow : !cartShow,
         });
     }
 
@@ -76,12 +83,14 @@ const Header = () => {
                 </motion.ul>
 
 
-                <div className='relative flex items-center justify-center'>
+                <div className='relative flex items-center justify-center' onClick={showCart}>
                     <MdShoppingBasket className='text-textColor text-2xl cursor-pointer'/>
-                    <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex 
-                    items-center justify-center'>
-                        <p className='text-xs text-white font-semibold'>2</p>
-                    </div>
+                    {cartItems && cartItems.length > 0 && (
+                        <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex 
+                        items-center justify-center'>
+                            <p className='text-xs text-white font-semibold'>{cartItems.length}</p>
+                        </div>
+                    )}
                 </div>
 
                 <div className='relative'>
@@ -125,12 +134,14 @@ const Header = () => {
 
         {/* mobie */}
         <div className='flex items-center justify-between md:hidden w-full h-full'>
-            <div className='relative flex items-center justify-center'>
+            <div className='relative flex items-center justify-center' onClick={showCart}>
                 <MdShoppingBasket className='text-textColor text-2xl cursor-pointer'/>
-                <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex 
-                items-center justify-center'>
-                    <p className='text-xs text-white font-semibold'>2</p>
-                </div>
+                {cartItems && cartItems.length > 0 && (
+                    <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex 
+                    items-center justify-center'>
+                        <p className='text-xs text-white font-semibold'>{cartItems.length}</p>
+                    </div>
+                )}
             </div>
 
             <Link to={'/'} className='flex items-center gap-2'>
--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -51,7 +51,7 @@ const HomeContainer = () => {
                         key={n.id}
                         className=' lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center
                         justify-center drop-shadow-lg'>
-                            <img src={n.imageSrc} className='w20 lg:w-40 -mt-10 lg:-mt-20' alt='I1'/>
+                            <img src={n.imageSrc} className='w-20 lg:w-40 -mt-10 lg:-mt-20' alt='I1'/>
                             <p className='text-base lg:text-xl font-semibold text-textColor mt-2 lg:mt-4'>
                             {n.name}
                             </p>
